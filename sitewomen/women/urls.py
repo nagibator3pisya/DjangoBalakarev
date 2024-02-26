b0a32f10a3855424ddf3b02ec3867a0c376ff2f4
Станислав <morevstas@mail.ru>
2024-02-26 13:31:25 +0300

    - заметки 1 последняя запись include - Диномеческие URL - Get Post запросы - перенаправление -доп перенаправлния - шаблоны
    - Шаблоны

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -5,6 +5,7 @@ register_converter(convertors.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index, name='home'), # http://127.0.0.1:8000 главная страница
+    path('about/', views.about, name='about'),
     path('cat/<int:cat_id>/', views.category, name='cat_id'), #http://127.0.0.1:8000/cat/1/
     path('cat/<slug:cat_slug>/', views.category_slug, name='cat_slug'),  #http://127.0.0.1:8000/cat/dd+d/
     path("archive/<year4:year>/", views.archive, name='archive')
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,28 +1,33 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 
 
-def index(requrest):
-    return HttpResponse("Страница приложения women")
+
+def index(request):
+    return render(request, "women/home.html")
 
 
-def category(requrest, cat_id):
+def about(request):
+    return render(request, 'women/about.html')
+
+
+def category(request, cat_id):
     return HttpResponse(f"<h1>Категории</h1>"
                         f"<p>id: {cat_id}</p>")
 
 
-def category_slug(requrest, cat_slug):
-    print(requrest.GET)
+def category_slug(request, cat_slug):
+    print(request.GET)
     return HttpResponse(f"<h1>Категории</h1>"
                         f"<p>slug: {cat_slug}</p>")
 
 
-def archive(requrest, year):
+def archive(request, year):
     if year > 2023:
         return redirect('home')
     return HttpResponse(f"<h1>Архив по годам</h1>"
                         f"<p>{year}</p>")
 
 
-def page_not_fount(requrest,exception):
+def page_not_fount(requestt,exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
