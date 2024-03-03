c75bcd0e8ae1635fdba9dfee417fd86ddeb66350
Станислав <morevstas02@mail.ru>
2024-03-03 20:02:40 +0300

    (ПОНЯЛ наследование шаблонов)

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -6,24 +6,26 @@ menu = [
     {'title': "О сайте",  'url_name': 'about'},
     {'title': "Добавить статью", 'url_name': 'add_page'},
     {'title': "Обратная связь", 'url_name': 'contact'},
-    {'title': "Войти", 'url_name': 'login'}
+    {'title': "Войти", 'url_name': 'login'},
+
 ]
 data_bd = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Всемирно известная актриса и гуманитарий.', 'is_published': True},
     {'id': 2, 'title': 'Джонни Депп', 'content': 'Талантливый актер и кинозвезда.', 'is_published': False},
     {'id': 3, 'title': 'Мэрилин Монро', 'content': 'Легендарная голливудская актриса и киноикона.', 'is_published': True}
 ]
+# Главная страница
 def index(request):
     data = {
         'title': "Главная станица",
         'menu': menu,
         'post': data_bd
     }
-    return render(request, "base.html", context=data)
+    return render(request, "women/home.html", context=data)
 
 
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
 def addpage(request):
@@ -36,6 +38,9 @@ def contact(request):
 
 def login(request):
     return HttpResponse(f"Авторизация")
+
+
+
 
 def how_post(request,post_id):
     return HttpResponse(f"Отображение статьи с id: {post_id}")
