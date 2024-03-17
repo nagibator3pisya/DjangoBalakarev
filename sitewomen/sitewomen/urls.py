16f541ff7afbd33a2f35aa0c5c54b5c41194d6b4
Станислав <morevstas02@mail.ru>
2024-03-17 17:21:29 +0300

    - заметки 1 последняя запись include - Диномеческие URL - Get Post запросы - перенаправление -доп перенаправлния - шаблоны - передача данных в шаблон(переменных) - Стандартные шаблонные фильтры - Шаблонный тег url - Наследование шаблонов - Создания новой ветки, test - заработало на ветки testv2 шаблоны - Модели, так же скопировал данные у балакирева, основа лежит на робочем столе

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -2,7 +2,7 @@
 URL configuration for sitewomen project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from women.views import page_not_fount
+from women import views
+from women.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('women.urls')), # http://127.0.0.1:8000
+    path('', include('women.urls')),
 ]
-handler404 = page_not_fount
+
+
+handler404 = page_not_found
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,51 +1,69 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
 
-
-menu = [
-    {'title': "О сайте",  'url_name': 'about'},
-    {'title': "Добавить статью", 'url_name': 'add_page'},
-    {'title': "Обратная связь", 'url_name': 'contact'},
-    {'title': "Войти", 'url_name': 'login'},
-
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
 ]
-data_bd = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Всемирно известная актриса и гуманитарий.', 'is_published': True},
-    {'id': 2, 'title': 'Джонни Депп', 'content': 'Талантливый актер и кинозвезда.', 'is_published': False},
-    {'id': 3, 'title': 'Мэрилин Монро', 'content': 'Легендарная голливудская актриса и киноикона.', 'is_published': True}
+
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
+    Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».''',
+     'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
 ]
-# Главная страница
+
+cats_db = [
+    {'id': 1, 'name': 'Актрисы'},
+    {'id': 2, 'name': 'Певицы'},
+    {'id': 3, 'name': 'Спортсменки'},
+]
+
+
 def index(request):
     data = {
-        'title': "Главная станица",
+        'title': 'Главная страница',
         'menu': menu,
-        'post': data_bd
+        'posts': data_db,
+        'cat_selected': 0,
     }
-    return render(request, "women/home.html", context=data)
+    return render(request, 'women/index.html', context=data)
 
 
 def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
 def addpage(request):
-    return HttpResponse(f"Добавление статьи")
+    return HttpResponse("Добавление статьи")
 
 
 def contact(request):
-    return HttpResponse(f"Обратная связь")
+    return HttpResponse("Обратная связь")
 
 
 def login(request):
-    return HttpResponse(f"Авторизация")
+    return HttpResponse("Авторизация")
 
 
+def show_category(request, cat_id):
+    data = {
+        'title': 'Отображение по рубоикам',
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=data)
 
 
-def how_post(request,post_id):
-    return HttpResponse(f"Отображение статьи с id: {post_id}")
-
-
-
-def page_not_fount(requestt,exception):
-    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
