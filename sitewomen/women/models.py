3eaa0ec9c5a69d89226ecc933994ac65201cdc00
Станислав <morevstas02@mail.ru>
2024-03-24 19:46:05 +0300

    - Знакомство с CRUD,удаление,изменение,+ класс мета

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -8,4 +8,15 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    is_published = models.BooleanField(default=True)
+
+
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        ordering = ['-title']
+        indexes = [
+            models.Index(fields=['-title'])
+        ]
