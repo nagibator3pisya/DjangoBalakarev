4e7a932872e066062f762737b67982e3177b9ed3
Станислав <morevstas02@mail.ru>
2024-03-25 20:18:22 +0300

    Сделали вывод по слагам

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Women(models.Model):
     title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255,unique=True,db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
+
+from women.models import Women
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -39,8 +41,15 @@ def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+    return render(request, 'women/post.html', context=data)
 
 
 def addpage(request):
