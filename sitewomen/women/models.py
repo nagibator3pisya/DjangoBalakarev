941a8053f7b662be637ca87747eb665ec3dea680
Станислав <morevstas02@mail.ru>
2024-03-26 22:02:56 +0300

    Пользовательский менеджер и Choices(выборку)

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,16 +1,33 @@
 from django.db import models
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    """
+    будет возвращать только публикованные записи
+    """
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
 
 
 
 class Women(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0,'Черновик'
+        PUBLISHED = 1, 'Опубликован'
+
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=255,unique=True,db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
 
 
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     def __str__(self):
         return self.title
@@ -21,3 +38,8 @@ class Women(models.Model):
         indexes = [
             models.Index(fields=['-title'])
         ]
+
+
+    def get_absolute_url(self):
+        """формирование юрл для каждой записи"""
+        return reverse('post', kwargs={'post_slug': self.slug})
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -12,14 +12,6 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Войти", 'url_name': 'login'}
 ]
 
-data_db = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
-    Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».''',
-     'is_published': True},
-    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
-    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
-]
-
 cats_db = [
     {'id': 1, 'name': 'Актрисы'},
     {'id': 2, 'name': 'Певицы'},
@@ -28,10 +20,11 @@ cats_db = [
 
 
 def index(request):
+    post = Women.published.all()
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': data_db,
+        'posts': post,
         'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=data)
